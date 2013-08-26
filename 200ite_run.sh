267204183b70bddd210afad265d225774b0800e7
Shogo Kawaguchi <kawaguchi@curiosity.(none)>
2013-08-26 10:46:40 +0900

    並列化修正

--- 200ite_run.sh
+++ 200ite_run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-nohup ./run_7_200ite.sh 1 &
-nohup ./run_7_200ite.sh 2 &
-nohup ./run_7_200ite.sh 3 &
-nohup ./run_7_200ite.sh 4 &
-nohup ./run_7_200ite.sh 5 &
+nohup ./run_7_200ite.sh 1 nagoya > nohup_1.out &
+sleep 1
+nohup ./run_7_200ite.sh 2 nagoya > nohup_2.out &
+sleep 1
+nohup ./run_7_200ite.sh 3 nagoya > nohup_3.out &
+sleep 1
+nohup ./run_7_200ite.sh 4 nagoya > nohup_4.out &
+sleep 1
+nohup ./run_7_200ite.sh 5 nagoya > nohup_5.out &
